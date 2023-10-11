78bbe142b67b74b6fe2669991bbec024fe5146a2
Jjman739 <Jjman739@gmail.com>
2023-10-11 18:45:58 -0400

    Basic freezing support

---
+++ Assets/Scripts/MonitorScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonitorScript : MonoBehaviour
+{
+    public Camera source;
+    private float freezeTime = 0;
+
+    public void Update() {
+          if (freezeTime > 0) {
+                freezeTime -= Time.deltaTime;
+                if (freezeTime <= 0) {
+                      source.enabled = true;
+                      Debug.Log("Unfroze camera: "+source.name);
+                }
+          }
+    }
+
+    public void Freeze() {
+          source.enabled = false;
+          Debug.Log("Froze camera: "+source.name);
+    }
+
+}
