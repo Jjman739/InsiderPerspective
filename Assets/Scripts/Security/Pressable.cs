deec885a7d08e17a8883ca134ea3bfc1cd863b59
Jjman739 <Jjman739@gmail.com>
2023-10-21 14:18:33 -0400

    Add Pressable abstract class.

---
+++ Assets/Scripts/Security/Pressable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Pressable : MonoBehaviour
+{
+    public bool pressed = false;
+
+    public abstract void Press();
+    public abstract void Unpress();
+}
