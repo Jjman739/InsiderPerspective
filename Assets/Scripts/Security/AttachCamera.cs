ef076debf23c3332f0850bfe5a8b79364ce5ac68
Ryan Dixon <rjd323@drexel.edu>
2023-10-24 16:28:02 -0400

    new map layout with lots o prefabs :)

---
+++ Assets/Scripts/Security/AttachCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachCamera : MonoBehaviour
+{
+    [SerializeField] private Camera attachedCamera;
+    private GameObject monitor;
+    private GameObject freezeButton;
+
+    private void Start()
+    {
+        monitor = transform.GetChild(0).gameObject;
+        freezeButton = transform.GetChild(1).gameObject;
+
+        RenderTexture texture = new RenderTexture(64, 64, 0);
+        monitor.GetComponent<MeshRenderer>().materials[0].SetTexture("_MainTex", texture);
+        attachedCamera.targetTexture = texture;
+        freezeButton.GetComponent<MonitorFreezeButton>().source = attachedCamera;
+    }
+}
