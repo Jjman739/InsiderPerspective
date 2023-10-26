3cd8c78eaaec40417e52c1b805a474e9c3a12956
Jjman739 <Jjman739@gmail.com>
2023-10-26 17:33:29 -0400

    Add indicator gauge

--- Assets/Scripts/Security/MonitorFreezeButton.cs
+++ Assets/Scripts/Security/MonitorFreezeButton.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class MonitorFreezeButton : Pressable
 {
       public Camera source;
+      public GameObject indicatorGauge;
       // How long to actually freeze for
       public float freezeDuration = 30;
       // How long it's currently frozen
       private float currentFreezeTime = 0;
+      private Vector3 gaugeBaseScale;
+
+      public void Start() {
+            gaugeBaseScale = indicatorGauge.transform.localScale;
+      }
 
       public void Update() {
             if (currentFreezeTime > 0) {
                   currentFreezeTime -= Time.deltaTime;
+                  indicatorGauge.transform.localScale = new Vector3(currentFreezeTime / freezeDuration, gaugeBaseScale.y, gaugeBaseScale.z);
                   if (currentFreezeTime <= 0) {
                         Debug.Log("Resumed camera: "+source.name);
                   }
+            } else {
+                  indicatorGauge.transform.localScale = new Vector3(0, gaugeBaseScale.y, gaugeBaseScale.z);
             }
       }
 
