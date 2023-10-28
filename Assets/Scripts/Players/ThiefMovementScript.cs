47b5e0dc0dfe1eef340fc5338ab28624f83f2b0f
ArjHawkins <ah3589@drexel.edu>
2023-10-28 19:29:08 -0400

    Jump and Win/Lose Scenes Added
    Added jump, win/lose scenes. Also added charges and a timer with reduces charges to the thief. Lose is triggered by hitting a trap or running out of charges. Win is currently impossible but it has a script for collecting enough treasures to load the win screen.

--- Assets/Scripts/Players/ThiefMovementScript.cs
+++ Assets/Scripts/Players/ThiefMovementScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ThiefMovementScript : MonoBehaviour
 {
@@ -11,8 +12,16 @@ public class ThiefMovementScript : MonoBehaviour
     private GameObject cameraHolder;
     private Camera mainCamera;
 
+    private float chargeTimer = 60;
+
+    private float jumpTimer;
+    private float jumpSpeed;
+
     [SerializeField] private float moveSpeed = 0.1f;
     [SerializeField] private float turnSpeed = 5f;
+    [SerializeField] private float jumpHeight = .5f;
+    [SerializeField] private float gravity = -10f;
+    [SerializeField] private int charge = 5;
 
     private void Start()
     {
@@ -26,8 +35,61 @@ public class ThiefMovementScript : MonoBehaviour
         move = new Vector3(0, 0, Input.GetAxis("ThiefMove"));
         twist = new Vector3(0, Input.GetAxis("ThiefTurn"), 0);
 
+        //jump control
+        bool grounded = controller.isGrounded;
+
+        if (grounded)
+        {
+            jumpTimer = .2f;
+        }
+
+        if (jumpTimer > 0)
+        {
+            jumpTimer -= Time.deltaTime;
+        }
+
+        if (grounded && jumpSpeed < 0)
+        {
+            jumpSpeed = 0f;
+        }
+
+        jumpSpeed += gravity * Time.deltaTime;
+
+        if (jumpTimer > 0 && Input.GetButton("Jump"))
+        {
+            jumpTimer = 0;
+            jumpSpeed = Mathf.Sqrt(jumpHeight * -gravity * 2);
+        }
+
+        move.y = jumpSpeed;
+
         controller.Move(transform.rotation * move * moveSpeed);
         transform.Rotate(turnSpeed * twist);
+
+
+        //timer to reduce charge
+        if (chargeTimer > 0)
+        {
+            chargeTimer -= Time.deltaTime;
+        }
+        else
+        {
+            chargeTimer = 60;
+            if(chargeTimer > 1)
+            {
+                chargeTimer -= 1;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.LoadScene("LoseScene");
     }
 
     public void SetMoveSpeed(float speed) { moveSpeed = speed; }
