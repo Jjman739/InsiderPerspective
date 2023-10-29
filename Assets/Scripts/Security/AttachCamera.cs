505c7a933b7a92e5c6e630bc37a2d07d0718d396
Jjman739 <Jjman739@gmail.com>
2023-10-28 21:31:59 -0400

    Increase monitor resolution

--- Assets/Scripts/Security/AttachCamera.cs
+++ Assets/Scripts/Security/AttachCamera.cs
@@ -13,7 +13,7 @@ public class AttachCamera : MonoBehaviour
         monitor = transform.GetChild(0).gameObject;
         freezeButton = transform.GetChild(1).gameObject;
 
-        RenderTexture texture = new RenderTexture(64, 64, 0);
+        RenderTexture texture = new RenderTexture(256, 256, 0);
         monitor.GetComponent<MeshRenderer>().materials[0].SetTexture("_MainTex", texture);
         attachedCamera.targetTexture = texture;
         freezeButton.GetComponent<MonitorFreezeButton>().source = attachedCamera;
