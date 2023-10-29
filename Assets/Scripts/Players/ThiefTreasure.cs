1a36e0c7c54006762012f1b3075fc0041f07a2f0
ArjHawkins <ah3589@drexel.edu>
2023-10-29 13:01:12 -0400

    Temp Lose Screen
    Score is tracked and thief gains score when they run into an object tagged treasure. Still going to lose screen instead of win so i changed the text.

--- Assets/Scripts/Players/ThiefTreasure.cs
+++ Assets/Scripts/Players/ThiefTreasure.cs
@@ -23,4 +23,15 @@ public class ThiefTreasure : MonoBehaviour
             SceneManager.LoadScene("WinScene");
         }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Treasure"))
+        {
+            ScoreTracker.control.treasureValue++;
+            treasureCount++;
+            Destroy(other.gameObject);
+            
+        }
+    }
 }
