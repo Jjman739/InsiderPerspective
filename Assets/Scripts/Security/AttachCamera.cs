56a42302c9aaa6e27b0bd56fc81a22468037ada4
Ryan Dixon <rjd323@drexel.edu>
2023-10-29 13:52:18 -0400

    add relic model, remove extra relic from tile room

--- Assets/Scripts/Security/AttachCamera.cs
+++ Assets/Scripts/Security/AttachCamera.cs
@@ -6,16 +6,13 @@ public class AttachCamera : MonoBehaviour
 {
     [SerializeField] private Camera attachedCamera;
     private GameObject monitor;
-    private GameObject freezeButton;
 
     private void Start()
     {
         monitor = transform.GetChild(0).gameObject;
-        freezeButton = transform.GetChild(1).gameObject;
 
         RenderTexture texture = new RenderTexture(64, 64, 0);
         monitor.GetComponent<MeshRenderer>().materials[0].SetTexture("_MainTex", texture);
         attachedCamera.targetTexture = texture;
-        freezeButton.GetComponent<MonitorFreezeButton>().source = attachedCamera;
     }
 }
