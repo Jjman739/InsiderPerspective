7710405bbd3b99d134b6a5d158f104e462cc5b2d
Jjman739 <Jjman739@gmail.com>
2023-11-04 21:15:13 -0400

    Update exit script

--- Assets/Scripts/Players/ThiefTreasure.cs
+++ Assets/Scripts/Players/ThiefTreasure.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class ThiefTreasure : MonoBehaviour
 {
     public int treasureCount = 0;
-
-    [SerializeField] private int goal = 1;
+    public int goal = 1;
 }
