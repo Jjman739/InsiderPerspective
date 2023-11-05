582502a7edc099ed3000c2b3793089f743cb2c78
C0smicBUndle <141589960+C0smicBUndle@users.noreply.github.com>
2023-11-05 10:54:43 -0500

    Added main menu

---
+++ Assets/Scripts/UI/UINavigation.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting the game...");
+        Application.Quit();
+    }
+}
