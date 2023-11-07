f3b121438aad2def53f29faf4c11cb759c32fd44
ArjHawkins <ah3589@drexel.edu>
2023-11-06 20:48:58 -0500

    Main Menu (In progress)
    Button works and theres light in scene. need to add instruction page. Committing before reinstall.

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : Component
 {
+
     private static T _instance;
     public static T Instance
     {
         get
         {
-            if (_instance == null)
+            /*if (_instance == null)
             {
                 _instance = FindObjectOfType<T>();
                 if (_instance == null)
                 {
                     return null;
                 }
-            }
+            }*/
             return _instance;
         }
     }
---
+++ Assets/Scripts/UI/SceneControl.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene current = SceneManager.GetActiveScene();
+
+        if (current.name != "SampleScene")
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
