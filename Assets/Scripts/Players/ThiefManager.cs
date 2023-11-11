3bc1a6db8eef87d909943ee17f03b6bcc4fa540b
Jjman739 <Jjman739@gmail.com>
2023-11-10 22:29:31 -0500

    Damage swaps two controls

--- Assets/Scripts/Players/ThiefManager.cs
+++ Assets/Scripts/Players/ThiefManager.cs
@@ -6,10 +6,35 @@ using UnityEngine.SceneManagement;
 public class ThiefManager : MonoBehaviour
 {
     [SerializeField] private Transform cameraRoot;
+    [SerializeField] private ThiefPhoto photo;
+    [SerializeField] private ThiefMovementScript movement;
 
     public void TakeDamage()
     {
         Debug.Log("Hit a trap.");
         cameraRoot.rotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+        ScrambleControls(ref movement.forwardButton, ref movement.backwardButton, ref movement.leftButton, ref movement.rightButton, ref movement.jumpButton, ref photo.photoButton);
+    }
+
+    private void ScrambleControls(ref string forward, ref string backward, ref string left, ref string right, ref string jump, ref string cam)
+    {
+        int buttonId1 = Random.Range(0,5);
+        int buttonId2 = Random.Range(0,4);
+        if (buttonId2 == buttonId1)
+        {
+            buttonId2++;
+        }
+
+        List<string> values = new List<string> { forward, backward, left, right, jump, cam };
+        string temp = values[buttonId1];
+        values[buttonId1] = values[buttonId2];
+        values[buttonId2] = temp;
+
+        forward = values[0];
+        backward = values[1];
+        left = values[2];
+        right = values[3];
+        jump = values[4];
+        cam = values[5];
     }
 }
