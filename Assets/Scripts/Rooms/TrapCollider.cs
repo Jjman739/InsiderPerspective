f54392efc7a368dd85b45900ebe90ef43bfa1257
Jjman739 <Jjman739@gmail.com>
2023-11-13 10:15:16 -0500

    Trap features now modular

--- Assets/Scripts/Rooms/TrapCollider.cs
+++ Assets/Scripts/Rooms/TrapCollider.cs
@@ -4,13 +4,17 @@ public class TrapCollider : MonoBehaviour
 {
     [SerializeField] private TrapToggle toggler;
 
+    public bool damage = true;
+    public bool selfDelete = true;
+    public bool alertGuard = false;
+
     void OnTriggerEnter(Collider other)
     {
         ThiefManager manager = other.gameObject.GetComponent<ThiefManager>();
         if (manager!= null)
         {
-            manager.TakeDamage();
-            toggler.TrapDisable();
+            if (damage) { manager.TakeDamage(); }
+            if (selfDelete) { toggler.TrapDisable(); }
         }
     }
 }
