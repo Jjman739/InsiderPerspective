c964275cf2b8036fd938ef0a45ab49ed2f67d8aa
Jjman739 <Jjman739@gmail.com>
2023-11-13 10:13:11 -0500

    Traps turn off when hit

--- Assets/Scripts/Rooms/TrapCollider.cs
+++ Assets/Scripts/Rooms/TrapCollider.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class TrapCollider : MonoBehaviour
 {
+    [SerializeField] private TrapToggle toggler;
+
     void OnTriggerEnter(Collider other)
     {
         ThiefManager manager = other.gameObject.GetComponent<ThiefManager>();
         if (manager!= null)
         {
             manager.TakeDamage();
+            toggler.TrapDisable();
         }
     }
 }
