084ca7b2639feddf1f0c25e9a591f5b191120edc
Jjman739 <Jjman739@gmail.com>
2023-11-18 21:23:23 -0500

    Remove local override for texture

--- Assets/Scripts/Security/AttachCamera.cs
+++ Assets/Scripts/Security/AttachCamera.cs
@@ -13,7 +13,7 @@ public class AttachCamera : MonoBehaviour
     {
         monitor = transform.GetChild(0).gameObject;
 
-        RenderTexture texture = new RenderTexture(256, 256, 0);
+        texture = new RenderTexture(256, 256, 0);
         monitor.GetComponent<MeshRenderer>().materials[0].SetTexture("_MainTex", texture);
         attachedCamera.targetTexture = texture;
         attachedCamera.enabled = true;
