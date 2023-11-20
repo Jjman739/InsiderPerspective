6cc6b9c9198b681a832ab99480dd66996aa246d0
basketini-jones <rjd323@drexel.edu>
2023-11-20 11:11:01 -0500

    fix TextureRandomizer and finish picture frame asset

--- Assets/Scripts/Rooms/TextureRandomizer.cs
+++ Assets/Scripts/Rooms/TextureRandomizer.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TextureSwap : MonoBehaviour
+public class TextureRandomizer : MonoBehaviour
 {
 
-    public Texture[] textures;
+    public Texture2D[] textures;
 
     // Start is called before the first frame update
     void Start()
