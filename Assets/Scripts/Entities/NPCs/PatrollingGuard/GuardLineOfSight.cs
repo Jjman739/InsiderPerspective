f29fd0d6dd8d48faf1caaa382a1cb1716c3f1b22
Jjman739 <Jjman739@gmail.com>
2023-11-30 23:14:14 -0500

    Add raycasting

--- Assets/Scripts/Entities/NPCs/PatrollingGuard/GuardLineOfSight.cs
+++ Assets/Scripts/Entities/NPCs/PatrollingGuard/GuardLineOfSight.cs
@@ -2,17 +2,32 @@ using UnityEngine;
 
 public class GuardLineOfSight : MonoBehaviour
 {
+    [SerializeField] private bool doRaycast = true;
+    [SerializeField] private Transform eyePoint;
+
+    private bool colliding = false;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            if (doRaycast)
+            {
+                bool hit = Physics.Raycast(other.gameObject.transform.position, eyePoint.position);
+                if (hit)
+                {
+                    return;
+                }
+            }
+
             other.gameObject.GetComponent<ThiefMovementScript>().SetInLineOfSight(true);
+            colliding = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (colliding && other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<ThiefMovementScript>().SetInLineOfSight(false);
         }
