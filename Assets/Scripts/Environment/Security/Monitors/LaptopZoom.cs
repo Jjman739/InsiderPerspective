1275629ec45c8b15a8296358ae969b15b87da6f4
ArjHawkins <ah3589@drexel.edu>
2023-12-07 18:03:00 -0500

    laptop ui always on now
    as described

--- Assets/Scripts/Environment/Security/Monitors/LaptopZoom.cs
+++ Assets/Scripts/Environment/Security/Monitors/LaptopZoom.cs
@@ -7,7 +7,6 @@ public class LaptopZoom : MonoBehaviour
 
     public Camera guardCam;
     public float defaultFov;
-    public GameObject laptopUI;
 
     // Start is called before the first frame update
     void Start()
@@ -27,14 +26,12 @@ public class LaptopZoom : MonoBehaviour
         {
             guardCam.fieldOfView = defaultFov / 2.5f;
             Cursor.lockState = CursorLockMode.None;
-            laptopUI.SetActive(true);
 
         }
         else if (Input.GetMouseButtonDown(1))
         {
             guardCam.fieldOfView = defaultFov;
             Cursor.lockState = CursorLockMode.Locked;
-            laptopUI.SetActive(false);
         }
 
 
